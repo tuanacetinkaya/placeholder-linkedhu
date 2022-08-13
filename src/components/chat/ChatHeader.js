6ec6416f6a26fdcfb8ddc82fd1085ed42f78a183
tuanacetinkaya <tuanacetinkaya7@gmail.com>
2022-08-13 04:15:40 +0300

    Updates

--- src/components/chat/ChatHeader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProfileBanner from "../commons/ProfileBanner";
-
-export default function ChatHeader({ user }) {
-  // return <div>Header</div>
-  console.log("chat header: ", user);
-  return <ProfileBanner user={user} contentType="chatHeader"></ProfileBanner>;
-}
